f18ebb99bb55b97d2c0aa06d732ef0a72ce350b5
Felipe Lima <felipinho.lima27@gmail.com>
2022-04-14 18:35:05 -0300

    finalizando todas as querys resolvers mutation

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -2,7 +2,7 @@ import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
 
 export default function Home() {
   const { user } = useUser();
-
+  console.log(user)
   return (
     <div>
       <pre>
